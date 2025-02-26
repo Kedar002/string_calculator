8b92d8acdd6bf01ebf5e6ec15d6b9e25c60b2018
Kedar Khopade <kedarkhopade@Kedars-MacBook-Air.local>
2025-02-26 12:25:03 +0530

    Added test case: Single number returns itself along with the old testcase

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -2,6 +2,10 @@ import 'package:test/test.dart';
 import '../lib/string_calculator.dart'; // This file doesn't exist yet, but we'll create it later
 
 void main() {
+  test('Empty string should return 0', () {
+    final calculator = StringCalculator();
+    expect(calculator.add(""), equals(0));
+  });
   test('Single number should return the number itself', () {
     final calculator = StringCalculator();
     expect(calculator.add("1"), equals(1));
