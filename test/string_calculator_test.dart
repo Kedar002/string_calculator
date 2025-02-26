c76befc507d73f0f6c73d826b3c092055cac9275
Kedar Khopade <kedarkhopade@Kedars-MacBook-Air.local>
2025-02-26 12:12:16 +0530

    Added first test case: Empty string returns 0

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -1,8 +1,9 @@
-import 'package:string_calculator/string_calculator.dart';
 import 'package:test/test.dart';
+import '../lib/string_calculator.dart'; // This file doesn't exist yet, but we'll create it later
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('Empty string should return 0', () {
+    final calculator = StringCalculator();
+    expect(calculator.add(""), equals(0));
   });
 }
